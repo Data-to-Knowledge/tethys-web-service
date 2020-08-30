f8117189b38fee32b4c29ee9e458bbb81a70eee2
mullenkamp <mullenkamp1@gmail.com>
2020-08-30 18:10:46 +1200

    update

--- app/main.py
+++ app/main.py
@@ -99,7 +99,7 @@ async def get_sites(dataset_id: str, geometry: Optional[Geo] = None, distance: f
         if geometry_dict['type'] == 'Polygon':
             s_filter.update({'geometry': {'$geoWithin': {'$geometry': geometry_dict}}})
         elif (geometry_dict['type'] == 'Point'):
-            if isinstance(distance, (float, int):
+            if isinstance(distance, (float, int)):
                 s_filter.update({'geometry': {'$nearSphere': {'$geometry': geometry_dict, '$maxDistance': distance}}})
             else:
                 return 'If a Point geometery is passed, then the distance parameter must be a float'
